a1fca0414520db4f3a211263ef87c0911813e338
Nuria <nuria@codegram.com>
2018-06-26 10:53:42 +0200

    disable vim config

--- src/os/create_local_config_files.sh
+++ src/os/create_local_config_files.sh
@@ -48,19 +48,19 @@ create_gitconfig_local() {
 
 }
 
-create_vimrc_local() {
-
-    declare -r FILE_PATH="$HOME/.vimrc.local"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if [ ! -e "$FILE_PATH" ]; then
-        printf "" >> "$FILE_PATH"
-    fi
-
-    print_result $? "$FILE_PATH"
-
-}
+# create_vimrc_local() {
+#
+#     declare -r FILE_PATH="$HOME/.vimrc.local"
+#
+#     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+#     if [ ! -e "$FILE_PATH" ]; then
+#         printf "" >> "$FILE_PATH"
+#     fi
+#
+#     print_result $? "$FILE_PATH"
+#
+# }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -70,7 +70,7 @@ main() {
 
     create_bash_local
     create_gitconfig_local
-    create_vimrc_local
+    # create_vimrc_local
 
 }
 
